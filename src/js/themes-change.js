74e567ad5c3982152d4711ce25859eaa482756db
Alexandra <64329428+Goobka@users.noreply.github.com>
2020-12-29 12:27:21 +0200

    Adds final settings

--- src/js/themes-change.js
+++ src/js/themes-change.js
@@ -2,19 +2,36 @@ import './refs';
 import { LIGHT, DARK } from './theme';
 import { load, save, remove } from './storage';
 
-
 function onCheckboxChange() {
-  refs.checkboxEl.checked ? onCheckboxChecked() : onCheckboxNotChecked();
+  event.currentTarget.checked ? onCheckboxChecked() : onCheckboxNotChecked();
 }
 
 function onCheckboxChecked() {
-    refs.bodyEl.classList.add(DARK);
-    refs.bodyEl.classList.remove(LIGHT);
+  refs.bodyEl.classList.add(DARK);
+  refs.bodyEl.classList.remove(LIGHT);
+  save('Theme', DARK);
+  refs.checkboxEl.checked = true;  
 }
 
 function onCheckboxNotChecked() {
-    refs.bodyEl.classList.add(LIGHT);
-    refs.bodyEl.classList.remove(DARK);
+  refs.bodyEl.classList.add(LIGHT);
+  refs.bodyEl.classList.remove(DARK);
+  save('Theme', LIGHT);
+  refs.checkboxEl.checked = false;
 }
 
-refs.checkboxEl.addEventListener('change', onCheckboxChange); 
+function currentTheme() {
+  const savedTheme = load('Theme');
+  if (savedTheme === LIGHT) {
+    onCheckboxNotChecked();
+    return;
+  }
+  if (savedTheme === DARK) {
+    onCheckboxChecked();
+    return;
+  }
+}
+
+currentTheme();
+
+refs.checkboxEl.addEventListener('change', onCheckboxChange);
